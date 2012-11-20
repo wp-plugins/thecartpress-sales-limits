b3e6697b521b9504ac06413b888dbc9be50a9f62
inigoini <inigoini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-20 11:27:06 +0000

    Tagging 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/thecartpress-sales-limits/tags/1.0.6@627650 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- SalesLimits.class.php
+++ SalesLimits.class.php
@@ -3,7 +3,7 @@
 Plugin Name: TheCartPress Sales Limits
 Plugin URI: http://extend.thecartpress.com/ecommerce-plugins/limits/
 Description: Sales Limits for TheCartPress
-Version: 1.0.3
+Version: 1.0.6
 Author: TheCartPress team
 Author URI: http://thecartpress.com
 License: GPL
@@ -27,42 +27,69 @@ Parent: thecartpress
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class TCPSalesLimits {
+define( 'TCP_LIMITS_FOLDER',		dirname( __FILE__ ) . '/' );
+define( 'TCP_LIMITS_ADMIN_FOLDER',	TCP_LIMITS_FOLDER . 'admin/' );
 
-	function init() {
-		if ( ! function_exists( 'is_plugin_active' ) ) require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-		if ( ! is_plugin_active( 'thecartpress/TheCartPress.class.php' ) ) add_action( 'admin_notices', array( $this, 'admin_notices' ) );
-		if ( function_exists( 'load_plugin_textdomain' ) ) load_plugin_textdomain( 'tcp_max', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+define( 'TCP_LIMITS_WEIGHT_COST',	'TCP_LIMITS_WEIGHT_COST' );
+define( 'TCP_LIMITS_PRICE_COST',	'TCP_LIMITS_PRICE_COST' );
+
+class SalesLimits {
+	function __construct() {
+		add_action( 'init', array( &$this, 'init' ) );
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		require_once( TCP_LIMITS_ADMIN_FOLDER .'MAXSettings.class.php' );
 	}
 
-	function admin_notices() { ?>
-		<div class="error">
-			<p><?php _e( '<strong>Sales Limits for TheCartPress</strong> requires TheCartPress plugin activated.', 'tcp_max' ); ?></p>
-		</div><?php
+	function init() {
+		if ( function_exists( 'load_plugin_textdomain' ) ) load_plugin_textdomain( 'tcp_max', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		if ( ! function_exists( 'is_plugin_active' ) ) require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		if ( ! is_plugin_active( 'thecartpress/TheCartPress.class.php' ) ) add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+
+		add_filter( 'tcp_checkout_validate_before_enter', array( &$this, 'tcp_checkout_validate_before_enter' ) );
+		add_filter( 'tcp_get_shopping_cart_summary', array( &$this, 'tcp_get_shopping_cart_summary' ), 10, 2 );
+		add_filter( 'tcp_get_shopping_cart_widget', array( &$this, 'tcp_get_shopping_cart_widget' ) );
+		add_filter( 'tcp_add_to_shopping_cart', array( &$this, 'tcp_add_to_shopping_cart' ) );
+		add_filter( 'tcp_modify_to_shopping_cart', array( &$this, 'tcp_add_to_shopping_cart' ) );
+	}
+
+	function admin_init() {
+		add_action( 'tcp_shopping_cart_summary_widget_form', array( &$this, 'tcp_shopping_cart_summary_widget_form' ), 10, 2 );
+		add_filter( 'tcp_shopping_cart_summary_widget_update', array( &$this, 'tcp_shopping_cart_summary_widget_update' ), 10, 2 );
+		add_action( 'tcp_shopping_cart_widget_form', array( &$this, 'tcp_shopping_cart_summary_widget_form' ), 10, 2 );
+		add_filter( 'tcp_shopping_cart_widget_update', array( &$this, 'tcp_shopping_cart_summary_widget_update' ), 10, 2 );
+		add_filter( 'plugin_action_links', array( &$this, 'plugin_action_links' ), 10, 2 );
+	}
+
+	function admin_notices() {
+		echo '<div class="error">
+			<p>', __( '<strong>Limits for TheCartPress</strong> requires TheCartPress plugin is activated.', 'tcp_max' ), '</p>
+		</div>';
 	}
 
 	function tcp_get_shopping_cart_summary( $html, $args ) {
 		$out = '';
 		if ( isset( $args['see_maximum_msg'] ) && $args['see_maximum_msg'] ) {
 			$shoppingcart = TheCartPress::getShoppingCart();
-			$max_settings = get_option( 'tcp_max_settings', array() );
-			$min_price = isset( $max_settings['min_price'] ) ? (float)$max_settings['min_price'] : 0;
-			$max_price = isset( $max_settings['max_price'] ) ? (float)$max_settings['max_price'] : 0;
-			$min_weight = isset( $max_settings['min_weight'] ) ? (float)$max_settings['min_weight'] : 0;
-			$max_weight = isset( $max_settings['max_weight'] ) ? (float)$max_settings['max_weight'] : 0;
+			global $thecartpress;
+			$min_price = (float)$thecartpress->get_setting( 'min_price', 0 );
+			$max_price = (float)$thecartpress->get_setting( 'max_price', 0 );
+			$fee_price = (float)$thecartpress->get_setting( 'fee_price', 0 );
+			$min_weight = (float)$thecartpress->get_setting( 'min_weight', 0 );
+			$max_weight = (float)$thecartpress->get_setting( 'max_weight', 0 );
+			$fee_weight = (float)$thecartpress->get_setting( 'fee_weight', 0 );
 			$weight = $shoppingcart->getWeight();
-			$total = $shoppingcart->getTotalToShow();
-			if ( $max_price > 0 && $total > $max_price ) {
-				$out .= '<li class="tcp_max_error exceed_price">' . $this->exceed_price() . '</li>';
-			}
-			if ( $total < $min_price ) {
-				$out .= '<li class="tcp_min_error not_reach_price">' . $this->not_reach_price() . '</li>';
-			}
-			if ( $max_weight > 0 && $weight > $max_weight ) {
+			$total = $shoppingcart->getTotal();
+			if ( $max_weight > 0 && $weight > $max_weight && $fee_price == 0 ) {
 				$out .= '<li class="tcp_max_error exceed_weight">' . $this->exceed_weight() . '</li>';
 			}
 			if ( $weight < $min_weight ) {
-				$out .= '<li class="tcp_min_error not_reach_weight">' . $this->not_reach_weight() . '</li>';
+				$out .= '<li class="tcp_min_fee not_reach_weight">' . $this->fee_weight() . '</li>';
+			}
+			if ( $max_price > 0 && $total > $max_price && $fee_weight == 0 ) {
+				$out .= '<li class="tcp_max_error exceed_price">' . $this->exceed_price() . '</li>';
+			}
+			if ( $total > 0 && $total < $min_price && $fee_price > 0 ) {
+				$out .= '<li class="fee_error not_reach_price">' . $this->fee_price() . '</li>';
 			}
 		}
 		return $html . $out;
@@ -77,54 +104,73 @@ class TCPSalesLimits {
 	 */
 	function tcp_checkout_validate_before_enter( $param ) {
 		$shoppingcart = TheCartPress::getShoppingCart();
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$min_price = isset( $max_settings['min_price'] ) ? (float)$max_settings['min_price'] : 0;
-		$max_price = isset( $max_settings['max_price'] ) ? (float)$max_settings['max_price'] : 0;
-		$min_weight = isset( $max_settings['min_weight'] ) ? (float)$max_settings['min_weight'] : 0;
-		$max_weight = isset( $max_settings['max_weight'] ) ? (float)$max_settings['max_weight'] : 0;
+		global $thecartpress;
+		$min_price = (float)$thecartpress->get_setting( 'min_price', 0 );
+		$max_price = (float)$thecartpress->get_setting( 'max_price', 0 );
+		$fee_price = (float)$thecartpress->get_setting( 'fee_price', 0 );
+		$min_weight = (float)$thecartpress->get_setting( 'min_weigt', 0 );
+		$max_weight = (float)$thecartpress->get_setting( 'max_weight', 0 );
+		$fee_weight = (float)$thecartpress->get_setting( 'fee_weight', 0 );
 		$weight = $shoppingcart->getWeight();
 		$total = $shoppingcart->getTotalToShow();
-		if ( $max_price > 0 && $total > $max_price ) {
-			$param['msg'][] = $this->exceed_price();
-			$param['validate'] = false;
-		}
-		if ( $total < $min_price ) {
-			$param['msg'][] = $this->not_reach_price();
-			$param['validate'] = false;
-		}
 		if ( $max_weight > 0 && $weight > $max_weight ) {
-			$param['msg'][] = $this->exceed_weight();
+			$param['msg'] = $this->exceed_weight();
 			$param['validate'] = false;
-		}
-		if ( $weight < $min_weight ) {
-			$param['msg'][] = $this->not_reach_weight();
+		} elseif ( $weight < $min_weight && $fee_weight == 0 ) {
+			$param['msg'] = $this->not_reach_weight();
 			$param['validate'] = false;
+		/*} elseif ( $weight < $min_weight && $fee_weight > 0 ) {
+			$shoppingcart->addOtherCost( TCP_LIMITS_WEIGHT_COST, $fee_weight );
+			$shoppingcart->deleteOtherCost( TCP_LIMITS_PRICE_COST );*/
+		} elseif ( $max_price > 0 && $total > $max_price ) {
+			$param['msg'] = $this->exceed_price();
+			$param['validate'] = false;
+		} elseif ( $total < $min_price  && $fee_price == 0 ) {
+			$param['msg'] = $this->not_reach_price();
+			$param['validate'] = false;
+		/*} elseif ( $weight < $min_weight && $fee_weight > 0) {
+			$shoppingcart->addOtherCost( TCP_LIMITS_PRICE_COST, $fee_price );
+			$shoppingcart->deleteOtherCost( TCP_LIMITS_WEIGHT_COST );*/
 		}
 		return $param;
 	}
 
 	function exceed_weight( $html = '' ) {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$max_weight = isset( $max_settings['max_weight'] ) ? (float)$max_settings['max_weight'] : 0;
+		global $thecartpress;
+		$max_weight = (float)$thecartpress->get_setting( 'max_weight', 0 );
 		return $html . '<span class="tcp_max_error excess_weight">' . sprintf( __( 'Sorry, orders over %1$s %2$s cannot be accepted', 'tcp_max' ), $max_weight, tcp_get_the_unit_weight() ) . '</span>';
 	}
 
 	function exceed_price( $html = '' ) {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$max_price = isset( $max_settings['max_price'] ) ? (float)$max_settings['max_price'] : 0;
+		global $thecartpress;
+		$max_price = (float)$thecartpress->get_setting( 'max_price', 0 );
 		return $html . '<span class="tcp_max_error excess_price">' . sprintf( __( 'Sorry, orders over %1$s cannot be accepted', 'tcp_max' ), tcp_format_the_price( $max_price ) ) . '</span>';
 
 	}
 
-	function not_reach_weight( $html = '' ) {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$min_weight = isset( $max_settings['min_weight'] ) ? (float)$max_settings['min_weight'] : 0;
-		return $html . '<span class="tcp_max_error not_reach_weight">' . sprintf( __( 'Sorry, orders under %1$s %2$s cannot be accepted', 'tcp_max' ), $min_weight, tcp_get_the_unit_weight() ) . '</span>';
+	function fee_weight( $html = '' ) {
+		global $thecartpress;
+		$min_weight = (float)$thecartpress->get_setting( 'min_weight', 0 );
+		$fee_weight = (float)$thecartpress->get_setting( 'fee_weight', 0 );
+		return $html . '<span class="tcp_fee_weight">' . sprintf( __( 'Sorry, orders under %1$s %2$s will be recharged with %3$s', 'tcp_max' ), $min_weight, tcp_get_the_unit_weight(), tcp_format_the_price( $fee_weight ) ) . '</span>';
 	}
 
+	function fee_price( $html = '' ) {
+		global $thecartpress;
+		$min_price = (float)$thecartpress->get_setting( 'min_price', 0 );
+		$fee_price = (float)$thecartpress->get_setting( 'fee_price', 0 );
+		return $html . '<span class="tcp_fee_weight">' . sprintf( __( 'Sorry, orders under %1$s will be recharged with %2$s', 'tcp_max' ), tcp_format_the_price( $min_price ), tcp_format_the_price( $fee_price ) ) . '</span>';
+	}
+
+	/*function not_reach_weight( $html = '' ) {
+		global $thecartpress;
+		$min_weight = (float)$thecartpress->get_setting( 'min_weight', 0 );
+		return $html . '<span class="tcp_max_error not_reach_weight">' . sprintf( __( 'Sorry, orders under %1$s %2$s cannot be accepted', 'tcp_max' ), $min_weight, tcp_get_the_unit_weight() ) . '</span>';
+	}*/
+
 	function not_reach_price( $html = '' ) {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$min_price = isset( $max_settings['min_price'] ) ? (float)$max_settings['min_price'] : 0;
+		global $thecartpress;
+		$min_price = (float)$thecartpress->get_setting( 'min_price', 0 );
 		return $html . '<span class="tcp_max_error not_reach_price">' . sprintf( __( 'Sorry, orders under %1$s cannot be accepted', 'tcp_max' ), tcp_format_the_price( $min_price ) ) . '</span>';
 	}
 
@@ -148,22 +194,29 @@ class TCPSalesLimits {
 		return $links;
 	}
 
-	function __construct() {
-		add_action( 'init', array( $this, 'init' ) );
-		if ( is_admin() ) {
-			require_once( dirname( __FILE__ ) .'/admin/MAXSettings.class.php' );
-			add_action( 'tcp_shopping_cart_summary_widget_form', array( $this, 'tcp_shopping_cart_summary_widget_form' ), 10, 2 );
-			add_filter( 'tcp_shopping_cart_summary_widget_update', array( $this, 'tcp_shopping_cart_summary_widget_update' ), 10, 2 );
-			add_action( 'tcp_shopping_cart_widget_form', array( $this, 'tcp_shopping_cart_summary_widget_form' ), 10, 2 );
-			add_filter( 'tcp_shopping_cart_widget_update', array( $this, 'tcp_shopping_cart_summary_widget_update' ), 10, 2 );
-			add_filter( 'plugin_action_links', array( $this, 'plugin_action_links' ), 10, 2 );
+	function tcp_add_to_shopping_cart( $shopping_cart_item ) {
+		$shoppingcart = TheCartPress::getShoppingCart();
+		global $thecartpress;
+		$min_weight = (float)$thecartpress->get_setting( 'min_weigt', 0 );
+		$fee_weight = (float)$thecartpress->get_setting( 'fee_weight', 0 );
+		$weight = $shoppingcart->getWeight();
+		if ( $weight < $min_weight && $fee_weight > 0 ) {
+			$shoppingcart->addOtherCost( TCP_LIMITS_WEIGHT_COST, $fee_weight, __( 'Minimum weight fee', 'tcp_max' ) );
 		} else {
-			add_filter( 'tcp_checkout_validate_before_enter', array( $this, 'tcp_checkout_validate_before_enter' ) );
-			add_filter( 'tcp_get_shopping_cart_summary', array( $this, 'tcp_get_shopping_cart_summary' ), 10, 2 );
-			add_filter( 'tcp_get_shopping_cart_widget', array( $this, 'tcp_get_shopping_cart_widget' ) );
+			$shoppingcart->deleteOtherCost( TCP_LIMITS_WEIGHT_COST );
 		}
+		$min_price = (float)$thecartpress->get_setting( 'min_price', 0 );
+		$fee_price = (float)$thecartpress->get_setting( 'fee_price', 0 );
+		$total = $shoppingcart->getTotalToShow();
+		if ( $total < $min_price && $fee_price > 0) {
+			$shoppingcart->addOtherCost( TCP_LIMITS_PRICE_COST, $fee_price, __( 'Minimum price fee', 'tcp_max' ) );
+		} else {
+			$shoppingcart->deleteOtherCost( TCP_LIMITS_PRICE_COST );
+		}
+		TheCartPress::saveShoppingCart();
+		return $shopping_cart_item;
 	}
 }
 
-new TCPSalesLimits();
+new SalesLimits();
 ?>
--- admin/MAXSettings.class.php
+++ admin/MAXSettings.class.php
@@ -16,80 +16,138 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class TCPMAXSettings {
+class TCPMaxSettings {
 
-	function admin_init() {
-		register_setting( 'tcp_max_options', 'tcp_max_settings', array( $this, 'validate' ) );
-		add_settings_section( 'tcp_max_main_section', __( 'Limits settings', 'tcp_max' ) , array( $this, 'show_tcp_max_section' ), __FILE__ );
-		add_settings_field( 'min_price', __( 'Minimum price', 'tcp_max' ), array( $this, 'show_min_price' ), __FILE__ , 'tcp_max_main_section' );
-		add_settings_field( 'max_price', __( 'Maximum price', 'tcp_max' ), array( $this, 'show_max_price' ), __FILE__ , 'tcp_max_main_section' );
-		add_settings_field( 'min_weight', __( 'Minimum weight', 'tcp_max' ), array( $this, 'show_min_weight' ), __FILE__ , 'tcp_max_main_section' );
-		add_settings_field( 'max_weight', __( 'Maximum weight', 'tcp_max' ), array( $this, 'show_max_weight' ), __FILE__ , 'tcp_max_main_section' );
+	private $updated = false;
+
+	function __construct() {
+		add_action( 'admin_menu', array( &$this, 'admin_menu' ), 99 );
+		global $tcp_miranda;
+		if ( $tcp_miranda ) $tcp_miranda->add_item( 'Limits', 'limits_settings', __( 'Limits', 'tcp_max' ), false, array( 'TCPMaxSettings', __FILE__ ) );
 	}
 
 	function admin_menu() {
+		if ( ! current_user_can( 'tcp_edit_settings' ) ) return;
 		global $thecartpress;
 		if ( $thecartpress ) {
-			$base = $thecartpress->get_base();
-			add_submenu_page( $base, __( 'Limits settings', 'tcp_max' ), __( 'Limits Settings', 'tcp_max' ), 'tcp_edit_settings', 'tcp_max_settings_page', array( $this, 'show_settings' ) );
+			$base = $thecartpress->get_base_settings();
+			$page = add_submenu_page( $base, __( 'Limits Setup', 'tcp_max' ), __( 'Limits Setup', 'tcp_max' ), 'tcp_edit_settings', 'max_setup', array( &$this, 'admin_page' ) );
+			//$page = add_submenu_page( $base, __( 'First Time Setup', 'tcp' ), __( 'First time', 'tcp' ), 'tcp_edit_settings', 'first_time_setup', array( &$this, 'admin_page' ) );
+			add_action( "load-$page", array( &$this, 'admin_load' ) );
+			add_action( "load-$page", array( &$this, 'admin_action' ) );
 		}
 	}
 
-	function show_settings() {?>
-	<div class="wrap">
-		<h2><?php _e( 'Maximum-Minimum Settings', 'tcp_max' );?></h2>
-		<form method="post" action="options.php">
-			<?php settings_fields( 'tcp_max_options' ); ?>
-			<?php do_settings_sections( __FILE__ ); ?>
-			<p class="submit">
-				<input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'tcp_max' ) ?>" />
-			</p>
-		</form>
-	</div><?php
+	function admin_load() {
+		get_current_screen()->add_help_tab( array(
+		    'id'      => 'overview',
+		    'title'   => __( 'Overview' ),
+		    'content' =>
+	            '<p>' . __( 'You can customize TheCartPress to only accept orders with a minimum or maximum price and weigth.', 'tcp_max' ) . '</p>',
+		) );
+
+		get_current_screen()->set_help_sidebar(
+			'<p><strong>' . __( 'For more information:', 'tcp' ) . '</strong></p>' .
+			'<p>' . __( '<a href="http://thecartpress.com" target="_blank">Documentation on TheCartPress</a>', 'tcp' ) . '</p>' .
+			'<p>' . __( '<a href="http://community.thecartpress.com/" target="_blank">Support Forums</a>', 'tcp' ) . '</p>' .
+			'<p>' . __( '<a href="http://extend.thecartpress.com/" target="_blank">Extend site</a>', 'tcp' ) . '</p>'
+		);
 	}
 
-	function validate( $input ) {
-		$input['min_price']		= isset( $input['min_price'] ) ? (float)$input['min_price'] : 0;
-		$input['max_price']		= isset( $input['max_price'] ) ? (float)$input['max_price'] : 0;
-		$input['min_weight']	= isset( $input['min_weight'] ) ? (float)$input['min_weight'] : 0;
-		$input['max_weight']	= isset( $input['max_weight'] ) ? (float)$input['max_weight'] : 0;
-		return $input;
+	function admin_page() { ?>
+<div class="wrap">
+	<?php screen_icon( 'tcp-max-min' ); ?><h2><?php _e( 'Limits Setup', 'tcp_max' ); ?></h2>
+<?php if ( !empty( $this->updated ) ) : ?>
+	<div id="message" class="updated">
+	<p><?php _e( 'Settings updated', 'tcp_max' ); ?></p>
+	</div>
+<?php endif; ?>
+<?php global $thecartpress;
+$min_price	= $thecartpress->get_setting( 'min_price', 0 );
+$fee_price	= $thecartpress->get_setting( 'fee_price', 0 );
+$max_price	= $thecartpress->get_setting( 'max_price', 0 );
+$min_weight	= $thecartpress->get_setting( 'min_weight', 0 );
+$fee_weight	= $thecartpress->get_setting( 'fee_weight', 0 );
+$max_weight	= $thecartpress->get_setting( 'max_weight', 0 ); ?>
+<form method="post" action="">
+<table class="form-table">
+<tbody>
+<tr valign="top">
+	<th scope="row">
+		<label for="min_price"><?php _e( 'Minimum price', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="min_price" name="min_price" value="<?php echo $min_price; ?>" size="10" maxlength="15" /> <?php tcp_the_currency(); ?>
+	</td>
+</tr>
+<tr valign="top">
+	<th scope="row">
+		<label for="fee_price"><?php _e( 'Small Order Fee', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="fee_price" name="fee_price" value="<?php echo $fee_price; ?>" size="10" maxlength="15" /> <?php tcp_the_currency(); ?>
+		<p class="description"><?php _e( 'This fee will be applicable if the minimum price is not exceeded. If this value is zero the minimum price must exceed to proceed to order.', 'tcp_max'); ?></p>
+	</td>
+</tr>
+<tr valign="top">
+	<th scope="row">
+		<label for="max_price"><?php _e( 'Maximum price', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="max_price" name="max_price" value="<?php echo $max_price; ?>" size="10" maxlength="15" /> <?php tcp_the_currency(); ?>
+	</td>
+</tr>
+<tr valign="top">
+	<th scope="row">
+		<label for="min_weight"><?php _e( 'Minimum weight', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="min_weight" name="min_weight" value="<?php echo $min_weight; ?>" size="10" maxlength="15" /> <?php tcp_the_unit_weight(); ?>
+	</td>
+</tr>
+<tr valign="top">
+	<th scope="row">
+		<label for="fee_weight"><?php _e( 'Small Order Fee', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="fee_weight" name="fee_weight" value="<?php echo $fee_weight; ?>" size="10" maxlength="15" /> <?php tcp_the_currency(); ?>
+		<p class="description"><?php _e( 'This fee will be applicable if the minimum weight is not exceeded. If this value is zero the minimum weight must exceed to proceed to order.', 'tcp_max'); ?></p>
+	</td>
+</tr>
+<tr valign="top">
+	<th scope="row">
+		<label for="max_weight"><?php _e( 'Maximum Weight', 'tcp_max' ); ?></label>
+	</th>
+	<td>
+		<input type="text" id="max_weight" name="max_weight" value="<?php echo $max_weight; ?>" size="10" maxlength="15" /> <?php tcp_the_unit_weight(); ?>
+	</td>
+</tr>
+<?php do_action( 'tcp_max_settings_page' ); ?>
+</tbody>
+</table>
+<?php wp_nonce_field( 'tcp_max_settings' ); ?>
+<?php submit_button( null, 'primary', 'save-max-settings' ); ?>
+</form>
+</div><?php
 	}
 
-	function show_tcp_max_section() {
-	}
-
-	function show_min_price() {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$min_price = isset( $max_settings['min_price'] ) ? (float)$max_settings['min_price'] : 0;?>
-		<input type="text" id="min_price" name="tcp_max_settings[min_price]" value="<?php echo $min_price;?>" size="10" maxlength="15" /> <?php tcp_the_currency();
-	}
-
-	function show_max_price() {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$max_price = isset( $max_settings['max_price'] ) ? (float)$max_settings['max_price'] : 0;?>
-		<input type="text" id="max_price" name="tcp_max_settings[max_price]" value="<?php echo $max_price;?>" size="10" maxlength="15" /> <?php tcp_the_currency();
-	}
-
-	function show_min_weight() {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$min_weight = isset( $max_settings['min_weight'] ) ? (float)$max_settings['min_weight'] : 0;?>
-		<input type="text" id="min_weight" name="tcp_max_settings[min_weight]" value="<?php echo $min_weight;?>" size="10" maxlength="15" /> <?php tcp_the_unit_weight();
-	}
-
-	function show_max_weight() {
-		$max_settings = get_option( 'tcp_max_settings', array() );
-		$max_weight = isset( $max_settings['max_weight'] ) ? (float)$max_settings['max_weight'] : 0;?>
-		<input type="text" id="max_weight" name="tcp_max_settings[max_weight]" value="<?php echo $max_weight;?>" size="10" maxlength="15" /> <?php tcp_the_unit_weight();
-	}
-	
-	function __construct() {
-		if ( is_admin() ) {
-			add_action('admin_init', array( $this, 'admin_init' ) );
-			add_action('admin_menu', array( $this, 'admin_menu' ), 99 );
-		}
+	function admin_action() {
+		if ( empty( $_POST ) ) return;
+		check_admin_referer( 'tcp_max_settings' );
+		$settings = get_option( 'tcp_settings' );
+		$settings['min_price']	= $_POST['min_price'];
+		$settings['fee_price']	= $_POST['fee_price'];
+		$settings['max_price']	= $_POST['max_price'];
+		$settings['min_weight']	= $_POST['min_weight'];
+		$settings['fee_weight']	= $_POST['fee_weight'];
+		$settings['max_weight']	= $_POST['max_weight'];
+		$settings = apply_filters( 'tcp_max_settings_action', $settings );
+		update_option( 'tcp_settings', $settings );
+		$this->updated = true;
+		global $thecartpress;
+		$thecartpress->load_settings();
 	}
 }
 
-new TCPMAXSettings();
+new TCPMaxSettings();
 ?>
